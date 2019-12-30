df74a0a105406f6d18aac3407a102c110760a2e8
Madis Väin <madisvain@gmail.com>
2019-12-30 23:45:30 +0200

    working particles

--- components/head.js
+++ components/head.js
@@ -12,9 +12,9 @@ const defaultOGImage = 'https://namespace.ee/images/namespace.png';
 
 const Head = props => (
   <NextHead>
-    <meta charset="utf-8" />
+    <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
     <title>{props.title || defaultTitle}</title>
     <meta name="description" content={props.description || defaultDescription} />
 
