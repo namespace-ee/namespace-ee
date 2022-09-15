c9d26ea8112774160a1df081dece866fa5e743ec
Madis Väin <madisvain@gmail.com>
2022-09-15 14:57:20 +0300

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,32 @@ import { get } from 'lodash';
 import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
 
+import { initGA, logPageView } from '../utils/ga';
+
 import '../styles/index.scss';
 
 const App = ({ Component, pageProps }) => {
   const cookies = parseCookies();
   const language = get(cookies, 'language', 'et');
+
+  useEffect(() => {
+    initGA();
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!router.asPath.includes('?')) {
+      logPageView();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    router.events.on('routeChangeComplete', logPageView);
+    return () => {
+      router.events.off('routeChangeComplete', logPageView);
+    };
+  }, [router.events]);
 
   useEffect(() => {
     async function load(language) {
